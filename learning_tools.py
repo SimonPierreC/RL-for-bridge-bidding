6d61764eaf2aad2c67624fc5c4055313a10f3844
Chardin Pierre <pierre.chardin@student-cs.fr>
2025-02-18 16:41:36 +0100

    ,nbjb

--- learning_tools.py
+++ learning_tools.py
@@ -29,27 +29,32 @@ def update(D, state, C, j):
     return D
 
 
-def perform_bids(x1, x2, scores,history, Q_models, nb_layers):
+def perform_bids(x1, x2, scores, history, Q_models, nb_layers):
     num_ones = np.count_nonzero(history == 1)
     bidding_history = history.copy()
     for i in range(num_ones, nb_layers):
-        hand = x1 if (i+1) % 2 == 1 else x2  # Détermine la main en fonction du joueur actif
-        state = torch.tensor(hand, dtype=torch.float32) if i == 0 else torch.tensor(np.concatenate([hand, bidding_history]), dtype=torch.float32)
+        # Détermine la main en fonction du joueur actif
+        hand = x1 if (i+1) % 2 == 1 else x2
+        state = torch.tensor(hand, dtype=torch.float32) if i == 0 else torch.tensor(
+            np.concatenate([hand, bidding_history]), dtype=torch.float32)
 
-        highest_bid = np.max(np.where(bidding_history == 1)) if np.any(bidding_history == 1) else -1
+        highest_bid = np.max(np.where(bidding_history == 1)) if np.any(
+            bidding_history == 1) else -1
 
         # passer l'état dans le modèle Q[i] et obtenir les valeurs Q[i]
         with torch.no_grad():
-            q_values = Q_models[i](state)  # prédiction du modèle Q[i] pour le state
+            # prédiction du modèle Q[i] pour le state
+            q_values = Q_models[i](state)
 
         q_values_masked = q_values.clone()
         q_values_masked[:highest_bid+1] = -float('inf')
-        
-        next_a = torch.argmax(q_values_masked).item()  # sélection greedy max(Q)   a mettre la regle next_a>derniere enchere
+
+        # sélection greedy max(Q)   a mettre la regle next_a>derniere enchere
+        next_a = torch.argmax(q_values_masked).item()
         bidding_history[next_a] = 1  # met à jour l'historique des enchères
 
         if next_a == len(bidding_history) - 1 or i == nb_layers - 1:
-            last_action = next_a  # sauvegarde de la dernière action 
+            last_action = next_a  # sauvegarde de la dernière action
             last_layer = i
             return scores[last_action], last_layer
 
@@ -67,7 +72,7 @@ def algo_p(action, x1, x2, scores, bidding_history, Q_models, nb_layers):
 def assess_models(Q_models, data_set):
     scores_layers = [0 for k in range(len(Q_models))]
     N_layers = [0 for k in range(len(Q_models))]
-    for k in tqdm(range(len(data_set))):
+    for k in range(len(data_set)):
         x1, x2 = data_set.iloc[k, :52], data_set.iloc[k, 52:104]
         r = data_set.iloc[k, 104:140]
         score, last_layer = perform_bids(
